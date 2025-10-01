ad4a91fcd53a263ca16d4119cdec861c3c2570c8
phu <phuthgce182003@fpt.edu.vn>
2025-10-01 14:30:45 +0700

    entity

---
+++ cvmanager/src/main/java/com/example/cvmanager/CvmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cvmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvmanagerApplication.class, args);
+	}
+
+}
---
+++ cvmanager/src/test/java/com/example/cvmanager/CvmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cvmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
